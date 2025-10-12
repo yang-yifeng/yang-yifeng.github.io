537fd850906f10edcb24d4168fc96d219775028e
yang-yifeng <103408362+yang-yifeng@users.noreply.github.com>
2025-10-12 19:10:11 +0000

    Deploying to gh-pages from @ yang-yifeng/yang-yifeng.github.io@ebd2296bd465bbd6433b4dd0a6e6056b4b98a4e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-beng-phd-fhea",
+    title: "BEng, PhD, FHEA",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
