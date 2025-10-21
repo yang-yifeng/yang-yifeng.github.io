8cbcbeff127b66f3a475c427e3ba4a49efaaee94
yang-yifeng <103408362+yang-yifeng@users.noreply.github.com>
2025-10-21 21:01:20 +0000

    Deploying to gh-pages from @ yang-yifeng/yang-yifeng.github.io@52134a335e0bdbfe8a1a7afd9af667a9d4cd3115 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -427,9 +427,9 @@ ninja.data = [{
           section: "News",},{id: "news-my-research-project-hyaice-dr-a-hybrid-hydrodynamic-ai-framework-for-predicting-ice-floe-drift-in-the-arctic-marginal-ice-zone-has-been-awarded-funding-from-the-nerc-arctic-office-under-the-supporting-impactful-uk-arctic-science-engagement-scheme-earth-africa-ocean-snowflake",
           title: 'My research project “HyAIce-Dr: A Hybrid Hydrodynamic–AI Framework for Predicting Ice Floe Drift...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
