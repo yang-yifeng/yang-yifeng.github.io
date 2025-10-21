ad08e44dfd4127199729ef02747d997c1aa27bcb
yang-yifeng <103408362+yang-yifeng@users.noreply.github.com>
2025-10-21 21:41:12 +0000

    Deploying to gh-pages from @ yang-yifeng/yang-yifeng.github.io@c01be20ec5d763ba9e72ddfaaad8596173e9a552 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -432,9 +432,9 @@ ninja.data = [{
           description: "A Hybrid Hydrodynamic–AI Framework for Predicting Ice Floe Drift in the Arctic Marginal Ice Zone",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-hydrodynamics-in-icy-water-environments",
+          title: 'Hydrodynamics in icy water environments',
+          description: "Focus on modelling and uncovering the physical mechanisms in the three-way coupling of water waves, floating ice, and offshore structures.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
